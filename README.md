6c60bea26afc30f21f67ce72c6d1bd1332709735
Toni G <toni.giorgino@isib.cnr.it>
2014-01-10 10:23:44 +0100

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,74 @@
 infix2pharmml
 =============
 
-Converter between mathematical infix notation and PharmML math markup
+Converter between mathematical infix notation and PharmML math markup.
+
+PharmML is a markup for pharmacometrics documented at http://pharmml.org/
+
+Toni G at ISIB CNR
+
+Notes
+-----
+
+
+This is preliminary and can be later packed in a stand-alone exe.
+Until then, you need e.g. activeperl and the modules indicated at the
+top of the source code. See notes therein.
+
+
+Usage
+-----
+
+Open a shell, then
+
+shell>   perl -w infix2mathml.pl
+
+Enter the expression to convert:
+sin(-2*t)
+About to parse: sin(-2*t)
+Parsed:         sin((-2) * t)
+XML:
+
+<Equation xmlns="http://www.pharmml.org/2013/03/Maths">
+  <Uniop op="sin">
+  <Binop op="times">
+  <Uniop op="minus">
+  <ct:Real>2</ct:Real>
+  </Uniop>
+  <ct:SymbRef symbIdRef="t"/>
+  </Binop>
+  </Uniop>
+</Equation>
+
+
+
+
+
+Syntax for custom function calls
+--------------------------------
+
+Due to a limitation in Symbolic::Math parser, calling user-defined function
+(with named arguments) is achieved like this:
+
+   call(combinedErrorModel,a=a,b=b,f=Cc)
+
+which should yield
+
+
+    <math:Equation>
+      <math:FunctionCall>
+	<ct:SymbRef symbIdRef="combinedErrorModel"/>
+	<math:FunctionArgument symbId="a">
+	  <ct:SymbRef symbIdRef="a"/>
+	</math:FunctionArgument>
+	<math:FunctionArgument symbId="b">
+	  <ct:SymbRef symbIdRef="b"/>
+	</math:FunctionArgument>
+	<math:FunctionArgument symbId="f">
+	  <math:Equation>
+	    <ct:SymbRef blkIdRef="main" symbIdRef="Cc"/>
+	  </math:Equation>
+	</math:FunctionArgument>
+      </math:FunctionCall>
+    </math:Equation>
+
---
+++ infix2mathml.pl
@@ -0,0 +1,167 @@
+use Math::Symbolic::Parser;
+use Math::Symbolic;
+use Math::Symbolic::ExportConstants qw/:all/;
+use Data::Dumper;
+use XML::Twig;
+
+use strict;
+
+my $using_call=0;
+
+# This will extend all parser objects in your program:
+# http://search.cpan.org/~smueller/Math-SymbolicX-ParserExtensionFactory-3.02/lib/Math/SymbolicX/ParserExtensionFactory.pm
+use Math::SymbolicX::ParserExtensionFactory (
+    call => sub {
+	my $argumentstring = shift;
+	my @as=split(',',$argumentstring);
+	my $opname=shift @as;
+	my @arguments=();
+	my @operands=();
+	$using_call++;
+	foreach my $arg (@as) {
+	    my ($arg,$val)=split('=',$arg);
+	    push @arguments,$arg;
+	    push @operands,$val;
+	}
+	my $result =  Math::Symbolic::Operator->new({
+	    custom_op => 1,
+	    custom_name => $opname,
+	    custom_arguments => \@arguments,
+	    custom_string => "$opname(@as)",
+	    operands => \@operands,
+						    }); 
+	return $result;
+    }  
+    );
+
+my $string='sin(call(pippo,a=2,b=cos(tan(t)+1)))';
+#my $string='sin(a)';
+
+
+# Limitations. 
+#  no logical operators
+#  many operators may be mismatched
+#  sqrt and exp are folded into power
+
+sub myarity {
+	my $x=shift;
+	my $p;
+	if($x->arity == 1) {
+		$p="Uniop";
+	} elsif($x->arity == 2) {
+		$p="Binop";
+	}
+	return $p;
+}
+
+# In Table 12.9-12.10
+# Math::Symbolic -> PharmML conversion table  
+sub renameop {
+	my $x=shift;
+	my %ot=( 
+		add=>'plus',
+		subtract=>'minus',
+		multiply=>'times',
+		division=>'divide',
+		exponentiate=>'power',
+		'log'=>'logx',
+		negate=>'minus', # pharmml minus is both arity-2 and arity-1
+	);
+	if(defined $ot{$x}) {
+		$x=$ot{$x};
+	} 
+	return $x;
+}
+
+# XMLify a custom operator
+sub function_call_open {
+    my $x=shift;
+    my $r="";
+    $r.="<math:FunctionCall>\n";
+    $r.="<!-- Translating: $x->{custom_string} -->\n";
+    $r.="<ct:SymbRef symbIdRef=\"".$x->{custom_name}."\"/>\n";
+    my $o=0;
+    foreach my $arg (@{$x->{custom_arguments}}) {
+	$r.="    <math:FunctionArgument symbId=\"$arg\">\n";
+	$r.=xmlify($x->{operands}->[$o++]);
+	$r.="    </math:FunctionArgument>\n";
+    }
+    return $r;
+}
+
+sub function_call_close {
+    return "</math:FunctionCall>\n";
+}
+
+
+
+sub xmlify {
+    my $tree=shift;
+    my $r="";
+
+    my $pre = sub {
+	my $x=shift;
+#	print Dumper($x);
+	if (defined $x->{custom_op}) {
+	    $r.= function_call_open($x);
+	    return undef;
+	} elsif($x->term_type == T_OPERATOR) {
+	    my $p=myarity($x);
+	    my $o=$x->type;
+	    my $os=$Math::Symbolic::Operator::Op_Types[$o]->{prefix_string};
+	    $os=renameop($os);
+	    $r.= "  <$p op=\"$os\">";
+	} elsif($x->term_type == T_VARIABLE) {
+		my $a=$x->name;
+		$r.= "  <ct:SymbRef symbIdRef=\"$a\"/>";
+	} elsif($x->term_type == T_CONSTANT) {
+		my $a=$x->to_string;
+		$r.= "  <ct:Real>$a</ct:Real>";
+	} else {
+		$r.= "Unknown: ".$x->to_string;
+	}
+	$r.= "\n";
+	return();
+    };
+
+    my $post = sub {
+	my $x=shift;
+	if (defined $x->{custom_op}) {
+	    $r.= function_call_close($x);
+	} elsif($x->term_type == T_OPERATOR) {
+		my $p=myarity($x);
+		$r.= "  </$p>\n";
+	} 
+	return();
+    };
+
+    $r.="<math:Equation xmlns=\"http://www.pharmml.org/2013/03/Maths\">\n";
+    $tree->descend(before=>$pre,after=>$post);
+    $r.="</math:Equation>\n";
+    return $r;
+}
+
+
+
+
+print "Enter the expression to convert:\n";
+my $string=<>;
+chomp $string; 
+
+print "About to parse:              $string\n";
+my $tree = Math::Symbolic->parse_from_string($string);
+defined $tree or die "Parse failure";
+
+print "Parsed [call() is broken]:   ".$tree->to_string."\n" unless $using_call;
+
+print "XML:\n\n";
+my $xml= xmlify($tree);
+
+my $twig=XML::Twig->new( pretty_print => 'indented'); 
+$twig->parse($xml);
+$twig->print;
+
+
+
+	
+
