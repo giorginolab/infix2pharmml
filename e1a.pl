c76898b6d0fbf2da0fa8e0f110ef867756d43a5f
Toni G <toni.giorgino@isib.cnr.it>
2014-05-13 23:16:06 +0200

    works simple

---
+++ e1a.pl
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+use Parse::RecDescent;
+
+sub e {
+    my $tag=shift;
+    my $c="@_";
+    return "<$tag>$c</$tag>\n";
+}
+
+
+ $::RD_ERRORS = 1; # Make sure the parser dies when it encounters an error
+ $::RD_WARN   = 1; # Enable warnings. This will warn on unused rules &c.
+ $::RD_HINT   = 1; # Give out hints to help fix problems.
+
+
+
+
+my $grammar = q {
+
+statement:      variable '=' statement   {$item [1] . "=" . $item [3]}
+              | expression               {$item [1]}
+
+expression:     term '+' expression      {main::e("plus",$item [1], $item [3])}
+              | term '-' expression      {main::e("minus",$item [1] , $item [3])}
+              | term
+
+term:           factor '*' term          {main::e("times",$item [1] , $item [3])}
+              | factor '/' term          {main::e("divide",$item [1] , $item [3])}
+              | factor
+
+factor:         number
+              | variable                 {main::e("var",$item[1])}
+              | '+' factor               {$item [2]}
+              | '-' factor               {main::e("neg",$item [2])}
+              | '(' statement ')'        {$item [2]}
+
+number:         /\d+/                    {main::e("int",$item[1])}
+
+variable:       /[a-z]+/i
+
+
+};
+
+my $parser=Parse::RecDescent->new($grammar);
+
+my $result = $parser->statement($ARGV[0]);
+print $result;
+
